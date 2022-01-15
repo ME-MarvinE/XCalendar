5cdfbc3b4c1df938cd12a5b8df555e14a6967df6
Marvin E <Marvin.Etuk@gmail.com>
2022-01-15 12:35:24 +0000

    Add calendar property controls to sample

--- XCalendarSample/XCalendarSample/ViewModels/MainPageViewModel.cs
+++ XCalendarSample/XCalendarSample/ViewModels/MainPageViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xamarin.CommunityToolkit.ObjectModel;
+using XCalendar.Enums;
 
 namespace XCalendarSample.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : BaseViewModel
     {
+        public DateTime? SelectedDate { get; set; } = DateTime.Today;
         public ObservableRangeCollection<DateTime> SelectedDates { get; set; } = new ObservableRangeCollection<DateTime>() { DateTime.Today };
+        public List<CalendarSelectionMode> SelectionModes { get; set; } = Enum.GetValues(typeof(CalendarSelectionMode)).Cast<CalendarSelectionMode>().ToList();
+        public List<CalendarPageStartMode> PageStartModes { get; set; } = Enum.GetValues(typeof(CalendarPageStartMode)).Cast<CalendarPageStartMode>().ToList();
+        public List<CalendarNavigationMode> NavigationModes { get; set; } = Enum.GetValues(typeof(CalendarNavigationMode)).Cast<CalendarNavigationMode>().ToList();
+        public List<CalendarNavigationLimitMode> NavigationLimitModes { get; set; } = Enum.GetValues(typeof(CalendarNavigationLimitMode)).Cast<CalendarNavigationLimitMode>().ToList();
+        public List<DayOfWeek> DaysOfWeek { get; set; } = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().ToList();
         public DateTime NavigatedDate { get; set; } = DateTime.Today;
     }
 }
