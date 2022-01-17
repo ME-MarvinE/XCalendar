7e31345d3e6f21e2e822a07dc1e72febf202af1c
Marvin E <Marvin.Etuk@gmail.com>
2022-01-17 03:33:57 +0000

    Add the ability to edit the DayNamesOrder in the sample

---
+++ XCalendarSample/XCalendarSample/Popups/ConstructListDialogPopup.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XCalendarSample.Popups
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConstructListDialogPopup : Popup<List<object>>
+    {
+        #region Properties
+        private List<object> _InitialItems { get; }
+
+        #region Bindable Properties
+        public ObservableRangeCollection<object> ReturnValueItems
+        {
+            get { return (ObservableRangeCollection<object>)GetValue(ReturnValueItemsProperty); }
+            set { SetValue(ReturnValueItemsProperty, value); }
+        }
+        public List<object> AvailableItems
+        {
+            get { return (List<object>)GetValue(AvailableItemsProperty); }
+            set { SetValue(AvailableItemsProperty, value); }
+        }
+        public object SelectedItem
+        {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        #region Bindable Properties Initialisers
+        public static readonly BindableProperty ReturnValueItemsProperty = BindableProperty.Create(nameof(ReturnValueItems), typeof(ObservableRangeCollection<object>), typeof(ConstructListDialogPopup), defaultValueCreator: ReturnValueItemsDefaultValueCreator);
+        public static readonly BindableProperty AvailableItemsProperty = BindableProperty.Create(nameof(AvailableItems), typeof(List<object>), typeof(ConstructListDialogPopup));
+        public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(ConstructListDialogPopup));
+        #endregion
+
+        #endregion
+
+        #region Commands
+        public ICommand DismissDialogCommand { get; set; }
+        public ICommand CancelDialogCommand { get; set; }
+        public ICommand ResetReturnValueItemsCommand { get; set; }
+        public ICommand AddSelectedItemCommand { get; set; }
+        public ICommand RemoveSelectedItemCommand { get; set; }
+        public ICommand ClearReturnValueItemsCommand { get; set; }
+        #endregion
+
+        #endregion
+
+        #region Constructors
+        public ConstructListDialogPopup(IEnumerable InitialItems, IEnumerable AvailableItems)
+        {
+            DismissDialogCommand = new Command(() => Dismiss(new List<object>(ReturnValueItems)));
+            CancelDialogCommand = new Command(CancelDialog);
+            ResetReturnValueItemsCommand = new Command(ResetReturnValueItems);
+            AddSelectedItemCommand = new Command(AddSelectedItem);
+            RemoveSelectedItemCommand = new Command<bool>(RemoveSelectedItem);
+            ClearReturnValueItemsCommand = new Command(ClearReturnValueItems);
+
+            InitializeComponent();
+
+            _InitialItems = InitialItems.Cast<object>().ToList();
+            this.AvailableItems = AvailableItems.Cast<object>().ToList();
+
+            ResetReturnValueItems();
+            SelectedItem = this.AvailableItems?.Count > 0 ? this.AvailableItems?[0] : null;
+        }
+        #endregion
+
+        #region Methods
+        public void CancelDialog()
+        {
+            Dismiss(_InitialItems);
+        }
+        public void ResetReturnValueItems()
+        {
+            ReturnValueItems.ReplaceRange(_InitialItems);
+        }
+        protected override List<object> GetLightDismissResult()
+        {
+            return _InitialItems;
+        }
+        public void AddSelectedItem()
+        {
+            if (SelectedItem != null)
+            {
+                ReturnValueItems.Add(SelectedItem);
+            }
+        }
+        public void RemoveSelectedItem(bool RemoveEveryOcurrence)
+        {
+            if (SelectedItem != null)
+            {
+                if (RemoveEveryOcurrence)
+                {
+                    ReturnValueItems.ReplaceRange(ReturnValueItems.Where(x => x != SelectedItem));
+                }
+                else
+                {
+                    ReturnValueItems.Remove(SelectedItem);
+                }
+            }
+        }
+        public void ClearReturnValueItems()
+        {
+            ReturnValueItems.Clear();
+        }
+
+        #region Bindable Properties Methods
+        private static object ReturnValueItemsDefaultValueCreator(BindableObject bindable)
+        {
+            return new ObservableRangeCollection<object>();
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- XCalendarSample/XCalendarSample/ViewModels/MainPageViewModel.cs
+++ XCalendarSample/XCalendarSample/ViewModels/MainPageViewModel.cs
@@ -1,27 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
 using XCalendar.Enums;
+using XCalendarSample.Popups;
 
 namespace XCalendarSample.ViewModels
 {
     public class MainPageViewModel : BaseViewModel
     {
+        #region Properties
         public DateTime? SelectedDate { get; set; } = DateTime.Today;
         public ObservableRangeCollection<DateTime> SelectedDates { get; } = new ObservableRangeCollection<DateTime>() { DateTime.Today };
-        public List<SelectionMode> SelectionModes { get; set; } = Enum.GetValues(typeof(SelectionMode)).Cast<SelectionMode>().ToList();
+        public List<XCalendar.Enums.SelectionMode> SelectionModes { get; set; } = Enum.GetValues(typeof(XCalendar.Enums.SelectionMode)).Cast<XCalendar.Enums.SelectionMode>().ToList();
         public List<PageStartMode> PageStartModes { get; set; } = Enum.GetValues(typeof(PageStartMode)).Cast<PageStartMode>().ToList();
         public List<NavigationMode> NavigationModes { get; set; } = Enum.GetValues(typeof(NavigationMode)).Cast<NavigationMode>().ToList();
         public List<NavigationLimitMode> NavigationLimitModes { get; set; } = Enum.GetValues(typeof(NavigationLimitMode)).Cast<NavigationLimitMode>().ToList();
-        public List<DayOfWeek> DaysOfWeek { get; set; } = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().ToList();
+        public List<DayOfWeek> DaysOfWeek { get; set; } = new List<DayOfWeek>()
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday,
+            DayOfWeek.Saturday,
+            DayOfWeek.Sunday
+        };
         public DateTime NavigatedDate { get; set; } = DateTime.Today;
         public DateTime DayRangeMinimumDate { get; set; } = DateTime.Today.AddYears(-5);
         public DateTime DayRangeMaximumDate { get; set; } = DateTime.Today.AddYears(5);
         public DayOfWeek StartOfWeek { get; set; } = DayOfWeek.Monday;
-        public SelectionMode SelectionMode { get; set; } = SelectionMode.Multiple;
+        public ObservableRangeCollection<DayOfWeek> DayNamesOrder { get; } = new ObservableRangeCollection<DayOfWeek>()
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday,
+            DayOfWeek.Saturday,
+            DayOfWeek.Sunday
+        };
+        public XCalendar.Enums.SelectionMode SelectionMode { get; set; } = XCalendar.Enums.SelectionMode.Multiple;
         public NavigationLimitMode NavigationLimitMode { get; set; } = NavigationLimitMode.LoopMinimumAndMaximumAndScopeToDayRange;
         public NavigationMode NavigationMode { get; set; } = NavigationMode.ByMonth;
         public PageStartMode PageStartMode { get; set; } = PageStartMode.NavigatedMonth;
+        #endregion
+
+        #region Commands
+        public ICommand ShowDayNamesOrderDialogCommand { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainPageViewModel()
+        {
+            ShowDayNamesOrderDialogCommand = new Command(ShowDayNamesOrderDialog);
+        }
+        #endregion
+
+        #region Methods
+        public async void ShowDayNamesOrderDialog()
+        {
+            IEnumerable<DayOfWeek> NewDayNamesOrder = (await Application.Current.MainPage.ShowPopupAsync(new ConstructListDialogPopup(DayNamesOrder, DaysOfWeek))).Cast<DayOfWeek>();
+            DayNamesOrder.ReplaceRange(NewDayNamesOrder);
+        }
+        #endregion
     }
 }
