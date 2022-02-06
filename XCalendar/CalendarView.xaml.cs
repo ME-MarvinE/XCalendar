265124cdd3c087d37f111887d5c9f2fe516f7776
Marvin E <Marvin.Etuk@gmail.com>
2022-02-06 02:55:30 +0000

    Add property 'NavigationBackgroundColor'

--- XCalendar/CalendarView.xaml.cs
+++ XCalendar/CalendarView.xaml.cs
@@ -441,6 +441,11 @@ namespace XCalendar
             get { return (SelectionType)GetValue(SelectionTypeProperty); }
             set { SetValue(SelectionTypeProperty, value); }
         }
+        public Color NavigationBackgroundColor
+        {
+            get { return (Color)GetValue(NavigationBackgroundColorProperty); }
+            set { SetValue(NavigationBackgroundColorProperty, value); }
+        }
 
         #region Bindable Properties Initialisers
         public static readonly BindableProperty NavigatedDateProperty = BindableProperty.Create(nameof(NavigatedDate), typeof(DateTime), typeof(CalendarView), DateTime.Now, defaultBindingMode: BindingMode.TwoWay, propertyChanged: NavigatedDatePropertyChanged, coerceValue: CoerceNavigatedDate);
@@ -498,6 +503,7 @@ namespace XCalendar
         public static readonly BindableProperty MonthViewHeightRequestProperty = BindableProperty.Create(nameof(MonthViewHeightRequest), typeof(double), typeof(CalendarView), 300d);
         public static readonly BindableProperty NavigationTemplateProperty = BindableProperty.Create(nameof(NavigationTemplate), typeof(ControlTemplate), typeof(CalendarView));
         public static readonly BindableProperty NavigationHeightRequestProperty = BindableProperty.Create(nameof(NavigationHeightRequest), typeof(double), typeof(CalendarView), 40d);
+        public static readonly BindableProperty NavigationBackgroundColorProperty = BindableProperty.Create(nameof(NavigationBackgroundColor), typeof(Color), typeof(CalendarView), Color.Transparent);
         public static readonly BindableProperty NavigationTextColorProperty = BindableProperty.Create(nameof(NavigationTextColor), typeof(Color), typeof(CalendarView), Color.Black);
         public static readonly BindableProperty NavigationArrowColorProperty = BindableProperty.Create(nameof(NavigationArrowColor), typeof(Color), typeof(CalendarView), Color.Black);
         public static readonly BindableProperty NavigationArrowBackgroundColorProperty = BindableProperty.Create(nameof(NavigationArrowBackgroundColor), typeof(Color), typeof(CalendarView), Color.Transparent);
