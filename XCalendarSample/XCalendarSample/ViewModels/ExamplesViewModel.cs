db90c3fbee5b9c01cb767de5a14322abc772465f
Marvin E <Marvin.Etuk@gmail.com>
2022-03-22 19:56:18 +0000

    Add 'Event Calendar' example to the sample app

--- XCalendarSample/XCalendarSample/ViewModels/ExamplesViewModel.cs
+++ XCalendarSample/XCalendarSample/ViewModels/ExamplesViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XCalendarSample.Views;
 
 namespace XCalendarSample.ViewModels
 {
     public class ExamplesViewModel : BaseViewModel
     {
+        #region Commands
+        public ICommand ShowEventCalendarExampleCommand { get; set; }
+        #endregion
+
+        #region Constructors
+        public ExamplesViewModel()
+        {
+            ShowEventCalendarExampleCommand = new Command(async () => await ShowEventCalendarExample());
+        }
+        #endregion
+
+        #region Methods
+        public async Task ShowEventCalendarExample()
+        {
+            await Shell.Current.Navigation.PushAsync(new EventCalendarExamplePage());
+        }
+        #endregion
     }
 }
---
+++ XCalendarSample/XCalendarSample/Views/EventCalendarExamplePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XCalendarSample.ViewModels;
+
+namespace XCalendarSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EventCalendarExamplePage : ContentPage
+    {
+        public EventCalendarExamplePage()
+        {
+            InitializeComponent();
+            BindingContext = new EventCalendarExampleViewModel();
+        }
+    }
+}
