e851c4477347374952b129043d5f663442701839
Marvin E <Marvin.Etuk@gmail.com>
2022-04-24 01:50:49 +0100

    Rename 'XCalendarSample' to 'XCalendarFormsSample'

---
+++ XCalendarFormsSample/XCalendarFormsSample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XCalendarFormsSample.Views;
+
+namespace XCalendarFormsSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XCalendarFormsSample/XCalendarFormsSample/Views/EventCalendarExamplePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XCalendarFormsSample.ViewModels;
+
+namespace XCalendarFormsSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EventCalendarExamplePage : ContentPage
+    {
+        public EventCalendarExamplePage()
+        {
+            InitializeComponent();
+            BindingContext = new EventCalendarExampleViewModel();
+        }
+    }
+}
