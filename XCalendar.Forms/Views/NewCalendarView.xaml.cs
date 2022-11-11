fff1788a570026f8239e85945f39c5f0d430c680
Marvin E <Marvin.Etuk@gmail.com>
2022-11-11 00:05:27 +0000

    Add functioning grid-based DaysView and CalendarView.
    The grid-based controls are called 'NewCalendarView' and 'NewDaysView' for now.

---
+++ XCalendar.Forms/Views/NewCalendarView.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XCalendar.Core.Interfaces;
+
+namespace XCalendar.Forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewCalendarView : ContentView
+    {
+        #region Properties
+
+        #region Bindable Properties
+        public DateTime NavigatedDate
+        {
+            get { return (DateTime)GetValue(NavigatedDateProperty); }
+            set { SetValue(NavigatedDateProperty, value); }
+        }
+        public IEnumerable<ICalendarDay> Days
+        {
+            get { return (IEnumerable<ICalendarDay>)GetValue(DaysProperty); }
+            set { SetValue(DaysProperty, value); }
+        }
+        public IList<DayOfWeek> DaysOfWeek
+        {
+            get { return (IList<DayOfWeek>)GetValue(DaysOfWeekProperty); }
+            set { SetValue(DaysOfWeekProperty, value); }
+        }
+        public ICommand ForwardsArrowCommand
+        {
+            get { return (ICommand)GetValue(ForwardsArrowCommandProperty); }
+            set { SetValue(ForwardsArrowCommandProperty, value); }
+        }
+        public object ForwardsArrowCommandParameter
+        {
+            get { return (object)GetValue(ForwardsArrowCommandParameterProperty); }
+            set { SetValue(ForwardsArrowCommandParameterProperty, value); }
+        }
+        public ICommand BackwardsArrowCommand
+        {
+            get { return (ICommand)GetValue(BackwardsArrowCommandProperty); }
+            set { SetValue(BackwardsArrowCommandProperty, value); }
+        }
+        public object BackwardsArrowCommandParameter
+        {
+            get { return (object)GetValue(BackwardsArrowCommandParameterProperty); }
+            set { SetValue(BackwardsArrowCommandParameterProperty, value); }
+        }
+        public ControlTemplate DayNamesTemplate
+        {
+            get { return (ControlTemplate)GetValue(DayNamesTemplateProperty); }
+            set { SetValue(DayNamesTemplateProperty, value); }
+        }
+        /// <summary>
+        /// The height of the view showing the days of the week.
+        /// </summary>
+        public double DayNamesHeightRequest
+        {
+            get { return (double)GetValue(DayNamesHeightRequestProperty); }
+            set { SetValue(DayNamesHeightRequestProperty, value); }
+        }
+        /// <summary>
+        /// The template used to display the days of the week.
+        /// </summary>
+        public DataTemplate DayNameTemplate
+        {
+            get { return (DataTemplate)GetValue(DayNameTemplateProperty); }
+            set { SetValue(DayNameTemplateProperty, value); }
+        }
+        public double DayNameVerticalSpacing
+        {
+            get { return (double)GetValue(DayNameVerticalSpacingProperty); }
+            set { SetValue(DayNameVerticalSpacingProperty, value); }
+        }
+        public double DayNameHorizontalSpacing
+        {
+            get { return (double)GetValue(DayNameHorizontalSpacingProperty); }
+            set { SetValue(DayNameHorizontalSpacingProperty, value); }
+        }
+        /// <summary>
+        /// The template used to display the <see cref="Days"/>.
+        /// </summary>
+        public ControlTemplate DaysViewTemplate
+        {
+            get { return (ControlTemplate)GetValue(DaysViewTemplateProperty); }
+            set { SetValue(DaysViewTemplateProperty, value); }
+        }
+        /// <summary>
+        /// The height of the view used to display the <see cref="Days"/>
+        /// </summary>
+        public double DaysViewHeightRequest
+        {
+            get { return (double)GetValue(DaysViewHeightRequestProperty); }
+            set { SetValue(DaysViewHeightRequestProperty, value); }
+        }
+        /// <summary>
+        /// The template used to display the view for navigating the calendar.
+        /// </summary>
+        public ControlTemplate NavigationViewTemplate
+        {
+            get { return (ControlTemplate)GetValue(NavigationViewTemplateProperty); }
+            set { SetValue(NavigationViewTemplateProperty, value); }
+        }
+        /// <summary>
+        /// The template used to display a <see cref="ICalendarDay"/>
+        /// </summary>
+        public ControlTemplate DayTemplate
+        {
+            get { return (ControlTemplate)GetValue(DayTemplateProperty); }
+            set { SetValue(DayTemplateProperty, value); }
+        }
+
+        #region Bindable Properties Initialisers
+        public static readonly BindableProperty NavigatedDateProperty = BindableProperty.Create(nameof(NavigatedDate), typeof(DateTime), typeof(NewCalendarView), DateTime.Today);
+        public static readonly BindableProperty DaysProperty = BindableProperty.Create(nameof(DaysProperty), typeof(IEnumerable<ICalendarDay>), typeof(NewCalendarView), propertyChanged: DaysPropertyChanged);
+        public static readonly BindableProperty DaysOfWeekProperty = BindableProperty.Create(nameof(DaysOfWeek), typeof(IList<DayOfWeek>), typeof(NewCalendarView), propertyChanged: DaysOfWeekPropertyChanged);
+        public static readonly BindableProperty ForwardsArrowCommandProperty = BindableProperty.Create(nameof(ForwardsArrowCommand), typeof(object), typeof(NewCalendarView));
+        public static readonly BindableProperty ForwardsArrowCommandParameterProperty = BindableProperty.Create(nameof(ForwardsArrowCommandParameter), typeof(object), typeof(NewCalendarView));
+        public static readonly BindableProperty BackwardsArrowCommandProperty = BindableProperty.Create(nameof(BackwardsArrowCommand), typeof(object), typeof(NewCalendarView));
+        public static readonly BindableProperty BackwardsArrowCommandParameterProperty = BindableProperty.Create(nameof(BackwardsArrowCommandParameter), typeof(object), typeof(NewCalendarView));
+        public static readonly BindableProperty DayTemplateProperty = BindableProperty.Create(nameof(DayTemplate), typeof(ControlTemplate), typeof(NewCalendarView));
+        public static readonly BindableProperty DayNamesTemplateProperty = BindableProperty.Create(nameof(DayNamesTemplate), typeof(ControlTemplate), typeof(NewCalendarView));
+        public static readonly BindableProperty DayNamesHeightRequestProperty = BindableProperty.Create(nameof(DayNamesHeightRequest), typeof(double), typeof(NewCalendarView), 25d);
+        public static readonly BindableProperty DayNameTemplateProperty = BindableProperty.Create(nameof(DayNameTemplate), typeof(DataTemplate), typeof(NewCalendarView));
+        public static readonly BindableProperty DayNameVerticalSpacingProperty = BindableProperty.Create(nameof(DayNameVerticalSpacing), typeof(double), typeof(NewCalendarView));
+        public static readonly BindableProperty DayNameHorizontalSpacingProperty = BindableProperty.Create(nameof(DayNameHorizontalSpacing), typeof(double), typeof(NewCalendarView));
+        public static readonly BindableProperty DaysViewTemplateProperty = BindableProperty.Create(nameof(DaysViewTemplate), typeof(ControlTemplate), typeof(NewCalendarView));
+        public static readonly BindableProperty DaysViewHeightRequestProperty = BindableProperty.Create(nameof(DaysViewHeightRequest), typeof(double), typeof(NewCalendarView), 300d);
+        public static readonly BindableProperty NavigationViewTemplateProperty = BindableProperty.Create(nameof(NavigationViewTemplate), typeof(ControlTemplate), typeof(NewCalendarView));
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Constructors
+        public NewCalendarView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+
+        #region Bindable Properties Methods
+        private static void DaysPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            NewCalendarView Control = (NewCalendarView)bindable;
+            IEnumerable<ICalendarDay> NewDays = (IEnumerable<ICalendarDay>)newValue;
+
+            Control.MainDaysView.Days = NewDays;
+        }
+        private static void DaysOfWeekPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            NewCalendarView Control = (NewCalendarView)bindable;
+            IList<DayOfWeek> NewDaysOfWeek = (IList<DayOfWeek>)newValue;
+
+            Control.MainDaysOfWeekView.ItemsSource = NewDaysOfWeek;
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ XCalendar.Forms/Views/NewDaysView.xaml.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XCalendar.Core.Interfaces;
+
+namespace XCalendar.Forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewDaysView : ContentView
+    {
+
+        #region Fields
+        private ControlTemplate _DefaultDayTemplate;
+        #endregion
+
+        #region Properties
+
+        #region Bindable Properties
+        public IEnumerable<ICalendarDay> Days
+        {
+            get { return (IEnumerable<ICalendarDay>)GetValue(DaysProperty); }
+            set { SetValue(DaysProperty, value); }
+        }
+        public IList<DayOfWeek> DaysOfWeek
+        {
+            get { return (IList<DayOfWeek>)GetValue(DaysOfWeekProperty); }
+            set { SetValue(DaysOfWeekProperty, value); }
+        }
+        /// <summary>
+        /// The template used to display a <see cref="ICalendarDay"/>
+        /// </summary>
+        public ControlTemplate DayTemplate
+        {
+            get { return (ControlTemplate)GetValue(DayTemplateProperty); }
+            set { SetValue(DayTemplateProperty, value); }
+        }
+        public GridLength ColumnWidth
+        {
+            get { return (GridLength)GetValue(ColumnWidthProperty); }
+            set { SetValue(ColumnWidthProperty, value); }
+        }
+        public GridLength RowHeight
+        {
+            get { return (GridLength)GetValue(RowHeightProperty); }
+            set { SetValue(RowHeightProperty, value); }
+        }
+
+        #region Bindable Properties Initialisers
+        public static readonly BindableProperty DaysProperty = BindableProperty.Create(nameof(DaysProperty), typeof(IEnumerable<ICalendarDay>), typeof(NewDaysView), propertyChanged: DaysPropertyChanged);
+        public static readonly BindableProperty DaysOfWeekProperty = BindableProperty.Create(nameof(DaysOfWeek), typeof(IList<DayOfWeek>), typeof(NewDaysView), propertyChanged: DaysOfWeekPropertyChanged);
+        public static readonly BindableProperty DayTemplateProperty = BindableProperty.Create(nameof(DayTemplate), typeof(ControlTemplate), typeof(NewDaysView), propertyChanged: DayTemplatePropertyChanged);
+        public static readonly BindableProperty ColumnWidthProperty = BindableProperty.Create(nameof(ColumnWidth), typeof(GridLength), typeof(NewDaysView), GridLength.Star);
+        public static readonly BindableProperty RowHeightProperty = BindableProperty.Create(nameof(RowHeight), typeof(GridLength), typeof(NewDaysView), GridLength.Star);
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Constructors
+        public NewDaysView()
+        {
+            InitializeComponent();
+            _DefaultDayTemplate = (ControlTemplate)Resources["DefaultDayTemplate"];
+        }
+        #endregion
+
+        #region Methods
+
+        public void UpdateDaysGrid()
+        {
+            if (Days == null || DaysOfWeek == null)
+            {
+                DaysGrid.Children.Clear();
+            }
+            else
+            {
+                IList DaysList = Days as IList;
+
+                if (DaysList == null)
+                {
+                    DaysList = Days.ToList();
+                }
+
+                int RequiredColumnDefinitionCount = DaysOfWeek.Count;
+                int RequiredRowDefinitionCount = (int)Math.Ceiling(DaysList.Count / (double)RequiredColumnDefinitionCount);
+                int RequiredAmountOfChildren = DaysList.Count;
+                bool ColumnDefinitionsChanged = false;
+                bool RowDefinitionsChanged = false;
+
+
+
+                //Add the required amount of columns to support the amount of days.
+                while (DaysGrid.ColumnDefinitions.Count != RequiredColumnDefinitionCount)
+                {
+                    if (DaysGrid.ColumnDefinitions.Count > RequiredColumnDefinitionCount)
+                    {
+                        DaysGrid.ColumnDefinitions.RemoveAt(DaysGrid.ColumnDefinitions.Count - 1);
+                    }
+                    else
+                    {
+                        DaysGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = ColumnWidth });
+                    }
+
+                    ColumnDefinitionsChanged = true;
+                }
+
+                //Add the required amount of rows to support the amount of days.
+                while (DaysGrid.RowDefinitions.Count != RequiredRowDefinitionCount)
+                {
+                    if (DaysGrid.RowDefinitions.Count > RequiredRowDefinitionCount)
+                    {
+                        DaysGrid.RowDefinitions.RemoveAt(DaysGrid.RowDefinitions.Count - 1);
+                    }
+                    else
+                    {
+                        DaysGrid.RowDefinitions.Add(new RowDefinition() { Height = RowHeight });
+                    }
+
+                    RowDefinitionsChanged = true;
+                }
+
+                //Remove the amount of views not needed for representing all days.
+                while (DaysGrid.Children.Count > RequiredAmountOfChildren)
+                {
+                    DaysGrid.Children.RemoveAt(DaysGrid.Children.Count - 1);
+                }
+
+                //Add the amount of views needed to represent all days.
+                while (DaysGrid.Children.Count < RequiredAmountOfChildren)
+                {
+                    View LastChild = DaysGrid.Children.Count == 0 ? null : DaysGrid.Children[DaysGrid.Children.Count - 1];
+
+                    int NewChildColumnIndex;
+                    int NewChildRowIndex;
+
+                    if (LastChild == null)
+                    {
+                        NewChildColumnIndex = 0;
+                        NewChildRowIndex = 0;
+                    }
+                    else
+                    {
+                        int LastChildColumnIndex = Grid.GetColumn(LastChild);
+                        int LastChildRowIndex = Grid.GetRow(LastChild);
+
+                        if (LastChildColumnIndex == DaysGrid.ColumnDefinitions.Count - 1)
+                        {
+                            NewChildColumnIndex = 0;
+                            NewChildRowIndex = LastChildRowIndex + 1;
+                        }
+                        else
+                        {
+                            NewChildColumnIndex = LastChildColumnIndex + 1;
+                            NewChildRowIndex = LastChildRowIndex;
+                        }
+                    }
+
+                    ContentView NewChild = new ContentView { ControlTemplate = DayTemplate ?? _DefaultDayTemplate };
+
+                    DaysGrid.Children.Add(NewChild, NewChildColumnIndex, NewChildRowIndex);
+                }
+
+                //Update the views' BindingContext so they can represent the collection of days.
+                for (int i = 0; i < DaysGrid.Children.Count; i++)
+                {
+                    ContentView Child = (ContentView)DaysGrid.Children[i];
+                    int NewChildColumnIndex =  i % RequiredColumnDefinitionCount;
+                    int NewChildRowIndex = (int)Math.Floor(i / (double)RequiredColumnDefinitionCount);
+
+                    Grid.SetColumn(Child, NewChildColumnIndex);
+                    Grid.SetRow(Child, NewChildRowIndex);
+
+                    Child.BindingContext = DaysList[i];
+                }
+            }
+        }
+
+        private void Days_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateDaysGrid();
+        }
+        private void DaysOfWeek_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateDaysGrid();
+        }
+
+        #region Bindable Properties Methods
+        private static void DaysPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            NewDaysView Control = (NewDaysView)bindable;
+
+            if (oldValue != null && oldValue is INotifyCollectionChanged ObservableOldDays)
+            {
+                ObservableOldDays.CollectionChanged -= Control.Days_CollectionChanged;
+            }
+
+            if (newValue is INotifyCollectionChanged ObservableNewDays)
+            {
+                ObservableNewDays.CollectionChanged += Control.Days_CollectionChanged;
+            }
+
+            Control.UpdateDaysGrid();
+        }
+        private static void DaysOfWeekPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            NewDaysView Control = (NewDaysView)bindable;
+
+            if (oldValue is INotifyCollectionChanged ObservableOldDaysOfWeek)
+            {
+                ObservableOldDaysOfWeek.CollectionChanged -= Control.DaysOfWeek_CollectionChanged; ;
+            }
+
+            if (newValue is INotifyCollectionChanged ObservableNewDaysOfWeek)
+            {
+                ObservableNewDaysOfWeek.CollectionChanged += Control.DaysOfWeek_CollectionChanged;
+            }
+
+            Control.UpdateDaysGrid();
+        }
+        private static void DayTemplatePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            NewDaysView Control = (NewDaysView)bindable;
+
+            ControlTemplate NewDayTemplate = (ControlTemplate)newValue;
+
+            for (int i = 0; i < Control.DaysGrid.Children.Count; i++)
+            {
+                ContentView Child = (ContentView)Control.DaysGrid.Children[i];
+
+                Child.ControlTemplate = NewDayTemplate ?? Control._DefaultDayTemplate;
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
