b152bb83048ebaca67a2f79f66344b8235ed1b18
Marvin E <Marvin.Etuk@gmail.com>
2023-01-08 19:45:11 +0000

    Change StringCharLimitConverter input value and parameter type to 'object' instead of 'int' in XCalendar.MAUI

--- XCalendar.Maui/Converters/StringCharLimitConverter.cs
+++ XCalendar.Maui/Converters/StringCharLimitConverter.cs
@@ -3,40 +3,43 @@ using System.Globalization;
 
 namespace XCalendar.Maui.Converters
 {
-    public class StringCharLimitConverter : BaseConverterOneWay<string, string, int>
+    public class StringCharLimitConverter : BaseConverterOneWay<object, string, object>
     {
+        private string _DefaultConvertReturnValue = "";
         public override string DefaultConvertReturnValue
         {
             get
             {
-                return "";
+                return _DefaultConvertReturnValue;
             }
             set
             {
+                _DefaultConvertReturnValue = value;
             }
         }
-        public override string ConvertFrom(string value, int parameter, CultureInfo culture)
+        public override string ConvertFrom(object value, object parameter, CultureInfo culture)
         {
             try
             {
-                int TargetLength = parameter;
+                string StringValue = value.ToString();
+                int TargetLength = Convert.ToInt32(parameter);
 
                 if (TargetLength == 0)
                 {
                     return "";
                 }
-                else if (TargetLength >= value.Length)
+                else if (TargetLength >= StringValue.Length)
                 {
-                    return value;
+                    return StringValue;
                 }
                 else
                 {
-                    return value.Substring(0, TargetLength);
+                    return StringValue.Substring(0, TargetLength);
                 }
             }
             catch
             {
-                return "";
+                return DefaultConvertReturnValue;
             }
         }
     }
