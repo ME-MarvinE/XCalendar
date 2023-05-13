75236249c57e86e974244a0f350659c46e4fbdcb
Marvin E <Marvin.Etuk@gmail.com>
2023-05-13 16:30:04 +0100

    Change DayView to use styles for each DayState it can be in instead of using specific customisation properties

---
+++ XCalendar.Forms/Styles/DefaultStyles.cs
@@ -0,0 +1,59 @@
+﻿using Xamarin.Forms;
+using XCalendar.Forms.Views;
+
+namespace XCalendar.Forms.Styles
+{
+    public static class DefaultStyles
+    {
+        #region Properties
+        public static Style DefaultDayViewCurrentMonthStyle { get; } = CreateDefaultDayViewCurrentMonthStyle();
+        public static Style DefaultDayViewOtherMonthStyle { get; } = CreateDefaultDayViewOtherMonthStyle();
+        public static Style DefaultDayViewTodayStyle { get; } = CreateDefaultDayViewTodayStyle();
+        public static Style DefaultDayViewSelectedStyle { get; } = CreateDefaultDayViewSelectedStyle();
+        public static Style DefaultDayViewInvalidStyle { get; } = CreateDefaultDayViewInvalidStyle();
+        #endregion
+
+        #region Methods
+        public static Style CreateDefaultDayViewCurrentMonthStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewOtherMonthStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.FromHex("#A0A0A0") });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewTodayStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewSelectedStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.FromHex("#E00000") });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewInvalidStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.FromHex("#FFA0A0") });
+
+            return style;
+        }
+        #endregion
+    }
+}
---
+++ XCalendar.Maui/Styles/DefaultStyles.cs
@@ -0,0 +1,58 @@
+﻿using XCalendar.Maui.Views;
+
+namespace XCalendar.Maui.Styles
+{
+    public static class DefaultStyles
+    {
+        #region Properties
+        public static Style DefaultDayViewCurrentMonthStyle { get; } = CreateDefaultDayViewCurrentMonthStyle();
+        public static Style DefaultDayViewOtherMonthStyle { get; } = CreateDefaultDayViewOtherMonthStyle();
+        public static Style DefaultDayViewTodayStyle { get; } = CreateDefaultDayViewTodayStyle();
+        public static Style DefaultDayViewSelectedStyle { get; } = CreateDefaultDayViewSelectedStyle();
+        public static Style DefaultDayViewInvalidStyle { get; } = CreateDefaultDayViewInvalidStyle();
+        #endregion
+
+        #region Methods
+        public static Style CreateDefaultDayViewCurrentMonthStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Colors.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Colors.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewOtherMonthStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Colors.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.FromArgb("#A0A0A0") });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewTodayStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Colors.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Colors.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewSelectedStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Color.FromArgb("#E00000") });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Colors.Black });
+
+            return style;
+        }
+        public static Style CreateDefaultDayViewInvalidStyle()
+        {
+            Style style = new Style(typeof(DayView)) { CanCascade = true };
+            style.Setters.Add(new Setter() { Property = VisualElement.BackgroundColorProperty, Value = Colors.Transparent });
+            style.Setters.Add(new Setter() { Property = DayView.TextColorProperty, Value = Color.FromArgb("#FFA0A0") });
+
+            return style;
+        }
+        #endregion
+    }
+}
