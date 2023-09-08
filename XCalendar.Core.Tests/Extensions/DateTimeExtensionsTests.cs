b7a98e7016e3ff461287206738a45aa068028087
Marvin E <Marvin.Etuk@gmail.com>
2023-09-08 17:45:36 +0100

    Fix DateTimeExtensions tests failing due to culture-specific parameters not being passed

--- XCalendar.Core.Tests/Extensions/DateTimeExtensionsTests.cs
+++ XCalendar.Core.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using XCalendar.Core.Extensions;
 using Xunit;
 
@@ -38,44 +39,44 @@ namespace XCalendar.Core.Tests.Extensions
         }
 
         [Theory]
-        [InlineData(2023, 9, 15, 2023, 9, 10)]
-        [InlineData(2024, 1, 7, 2024, 1, 7)]
-        [InlineData(2022, 12, 25, 2022, 12, 25)]
+        [InlineData(2023, 9, 15, DayOfWeek.Sunday, 2023, 9, 10)]
+        [InlineData(2024, 1, 7, DayOfWeek.Sunday, 2024, 1, 7)]
+        [InlineData(2022, 12, 25, DayOfWeek.Sunday, 2022, 12, 25)]
         public void FirstDayOfWeekShouldReturnCorrectDate(
-            int year, int month, int day,
+            int year, int month, int day, DayOfWeek dayOfWeek,
             int expectedYear, int expectedMonth, int expectedDay)
         {
             var date = new DateTime(year, month, day);
 
-            var result = date.FirstDayOfWeek();
+            var result = date.FirstDayOfWeek(dayOfWeek);
 
             Assert.Equal(new DateTime(expectedYear, expectedMonth, expectedDay), result);
         }
 
         [Theory]
-        [InlineData(2023, 9, 15, 2023, 9, 16)]
-        [InlineData(2024, 1, 7, 2024, 1, 13)]
-        [InlineData(2022, 12, 25, 2022, 12, 31)]
+        [InlineData(2023, 9, 15, DayOfWeek.Sunday, 2023, 9, 16)]
+        [InlineData(2024, 1, 7, DayOfWeek.Sunday, 2024, 1, 13)]
+        [InlineData(2022, 12, 25, DayOfWeek.Sunday, 2022, 12, 31)]
         public void LastDayOfWeekShouldReturnCorrectDate(
-            int year, int month, int day,
+            int year, int month, int day, DayOfWeek dayOfWeek,
             int expectedYear, int expectedMonth, int expectedDay)
         {
             var date = new DateTime(year, month, day);
 
-            var result = date.LastDayOfWeek();
+            var result = date.LastDayOfWeek(dayOfWeek);
 
             Assert.Equal(new DateTime(expectedYear, expectedMonth, expectedDay), result);
         }
 
         [Theory]
-        [InlineData(2023, 9, 15, 3)]
-        [InlineData(2024, 1, 7, 2)]
-        [InlineData(2022, 12, 25, 5)]
-        public void CalendarWeekOfMonthShouldReturnCorrectWeek(int year, int month, int day, int expectedWeekOfMonth)
+        [InlineData(2023, 9, 15, DayOfWeek.Sunday, 3)]
+        [InlineData(2024, 1, 7, DayOfWeek.Sunday, 2)]
+        [InlineData(2022, 12, 25, DayOfWeek.Sunday, 5)]
+        public void CalendarWeekOfMonthShouldReturnCorrectWeek(int year, int month, int day, DayOfWeek dayOfWeek, int expectedWeekOfMonth)
         {
             var date = new DateTime(year, month, day);
 
-            var result = date.CalendarWeekOfMonth();
+            var result = date.CalendarWeekOfMonth(dayOfWeek);
 
             Assert.Equal(expectedWeekOfMonth, result);
         }
